f239422f081e4918779b6775b1f4e53affcaa6d1
Richard Mipana <richard.mipana@fullsail.edu>
2016-03-03 16:49:27 -0600

    deleted unnecessary lines

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,11 +10,6 @@ function initMap() {
   coordInfoWindow.setContent(createInfoWindowContent(chicago, map.getZoom()));
   coordInfoWindow.setPosition(chicago);
   coordInfoWindow.open(map);
-
-  // map.addListener('zoom_changed', function() {
-  //   coordInfoWindow.setContent(createInfoWindowContent(chicago, map.getZoom()));
-  //   coordInfoWindow.open(map);
-  // });
 }
 
 // var TILE_SIZE = 256;
@@ -40,17 +35,3 @@ function createInfoWindowContent(latLng, zoom) {
     '</div>'
   ].join('');
 }
-
-// The mapping between latitude, longitude and pixels is defined by the web
-// mercator projection.
-// function project(latLng) {
-//   var siny = Math.sin(latLng.lat() * Math.PI / 180);
-
-  // Truncating to 0.9999 effectively limits latitude to 89.189. This is
-  // about a third of a tile past the edge of the world tile.
-  // siny = Math.min(Math.max(siny, -0.9999), 0.9999);
-
-  // return new google.maps.Point(
-  //     TILE_SIZE * (0.5 + latLng.lng() / 360),
-  //     TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI)));
-//}
